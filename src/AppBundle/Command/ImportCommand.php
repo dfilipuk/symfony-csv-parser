04e48677f2536e15216d76874160d208a067aa36
Dzmitry Filipuk <dzmitry@filipuk.me>
2018-04-06 09:24:19 +0300

    implemented test mode option

--- src/AppBundle/Command/ImportCommand.php
+++ src/AppBundle/Command/ImportCommand.php
@@ -4,12 +4,15 @@ namespace AppBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ImportCommand extends ContainerAwareCommand
 {
     private const COMMAND_NAME = 'csv-parser:import';
     private const COMMAND_DESCRIPTION = 'Import data from CSV file to database';
+    private const COMMAND_TEST_MODE_OPTION_NAME = 'test';
+    private const COMMAND_TEST_MODE_DESCRIPTION = 'If set, everything will be performed except inserting data to the database';
 
     /**
      * {@inheritdoc}
@@ -17,7 +20,13 @@ class ImportCommand extends ContainerAwareCommand
     protected function configure()
     {
         $this->setName(self::COMMAND_NAME)
-            ->setDescription(self::COMMAND_DESCRIPTION);
+            ->setDescription(self::COMMAND_DESCRIPTION)
+            ->addOption(
+                self::COMMAND_TEST_MODE_OPTION_NAME,
+                null,
+                InputOption::VALUE_NONE,
+                self::COMMAND_TEST_MODE_DESCRIPTION
+            );
     }
 
     /**
@@ -25,6 +34,9 @@ class ImportCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        echo sprintf("%s: %s\n", self::COMMAND_NAME, 'Hello, world!');
+        $isTestOptionSet = $input->getOption(self::COMMAND_TEST_MODE_OPTION_NAME);
+        $mode = $isTestOptionSet ? 'test' : 'working';
+
+        $output->writeln(sprintf("%s: %s mode", self::COMMAND_NAME, $mode));
     }
 }
